fdb4dafd2bdea67dee1d651aa6576dad69f880b1
gurusoft13 <49583931+gurusoft13@users.noreply.github.com>
2020-05-03 21:08:38 +0300

    Update: ParticipantInfo attached

--- client/src/components/Party/Participant/index.jsx
+++ client/src/components/Party/Participant/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 // import ParticipantInfo from '../ParticipantInfo/ParticipantInfo'
 import ParticipantTracks from './ParticipantTracks'
-
+import ParticipantInfo from './ParticipantInfo'
 export default function Participant({ participant, disableAudio }) {
   return (
-    // <ParticipantInfo
-    //   participant={participant}
-    //   onClick={onClick}
-    //   isSelected={isSelected}
-    // >
-    <ParticipantTracks
+    <ParticipantInfo
       participant={participant}
-      disableAudio={disableAudio}
-      // enableScreenShare={enableScreenShare}
-    />
-    // </ParticipantInfo>
+      // onClick={onClick}
+      // isSelected={isSelected}
+    >
+      <ParticipantTracks
+        participant={participant}
+        disableAudio={disableAudio}
+        // enableScreenShare={enableScreenShare}
+      />
+    </ParticipantInfo>
   )
 }
